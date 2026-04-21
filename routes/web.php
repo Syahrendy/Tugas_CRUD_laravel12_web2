9fbaeb9bcebdfe2b923c832a5be89e2f934b3233
Syahrendy Fazri <syahrendyfazri01@gmail.com>
2026-04-22 02:56:52 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', [MahasiswaController::class, 'index']);
+Route::resource('mahasiswa', MahasiswaController::class);
